4045362bf1e985e32dfe7fc73e8efd4962f6f9d6
YTRodi <yagorodi@gmail.com>
2020-08-29 21:38:39 -0300

    Ej 18 terminado

---
+++ Ejercicios de la guia/POO/ej18/Clases/Garage.php
@@ -0,0 +1,89 @@
+<?php
+//Tengo que incluir Auto.php? no?
+//include "./Auto.php";
+
+class Garage {
+    //Atributos
+    private $_razonSocial;
+    private $_precioPorHora;
+    private $_autos;
+    
+    //Constructores
+    public function __construct(string $rs = "", float $ph = 0) {
+        $this->_razonSocial = $rs;
+        $this->_precioPorHora = $ph;
+        $this->_autos = [];
+    }
+
+    //Métodos
+    public function MostrarGarage(){
+        $infoGarage = "";
+        $i = 0;
+
+        $infoGarage .= "Razón social: {$this->_razonSocial}</br>";
+        $infoGarage .= "Precio por hora: $ {$this->_precioPorHora}</br>";
+
+        $infoGarage .= "Datos autos en garage:</br>";
+
+        foreach ($this->_autos as $key) {
+            $infoGarage .= "</br>Auto nº {$i}:";
+            $infoGarage .= $key->MostrarAuto($key);
+            $i++;
+        }
+
+        $infoGarage .= "</hr>";
+
+        return $infoGarage;
+    }
+
+    public function Equals(Garage $garage, Auto $auto){
+        if(in_array($auto,$garage->_autos)){
+            return "True"; //true;
+        }else{
+            return "False"; //false;
+        }
+    }
+
+    public function Add(Auto $auto){
+        $infoAdd = "";
+
+        if($auto !== null){
+            if(!in_array($auto,$this->_autos)){
+                array_push($this->_autos,$auto);
+                $infoAdd .= "Auto agregado con éxito! </br></br>";
+
+            }else{
+                $infoAdd .= "El auto ya se encuentra en el garage.</br></br>";                
+            }
+
+        }else{
+            $infoAdd .= "El auto es null. </br>";
+        }
+
+        return $infoAdd;
+    }
+
+    public function Remove(Auto $auto){
+        $infoRemove = "";
+
+        if($auto !== null){
+            if(in_array($auto,$this->_autos)){
+                foreach ($this->_autos as $key => $value) {
+                    if($this->_autos[$key] === $auto){
+                        unset($this->_autos[$key]);
+                        $infoRemove .= "</br>Auto eliminado con éxito! </br></br>";
+                        break;
+                    }
+                }
+
+            }else{
+                $infoRemove .= "El auto no está en el garage.</br>";
+            }
+
+        }else{
+            $infoRemove .= "El auto es null. </br>";
+        }
+        
+        return $infoRemove;
+    }
+}
---
+++ Ejercicios de la guia/POO/ej18/testGarage.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once "../ej18/Clases/Garage.php";
+include_once "../ej18/Clases/Auto.php";
+
+$garage = new Garage("Pikachu garage",170);
+$a1 = new Auto("Mitsubishi", "Azul", 500000, new DateTime("now"));
+$a2 = new Auto("Ford", "Negro", 1000000);
+$a3 = new Auto("Fiat", "Azul");
+
+echo $garage->Add($a1);
+echo $garage->Add($a1);
+echo $garage->Add($a2);
+echo $garage->Add($a3);
+
+echo $garage->MostrarGarage();
+echo $garage->Remove($a2); //Falta testear esta función
+//var_dump($garage);
+echo $garage->MostrarGarage();
+echo $garage->Add($a2);
+echo $garage->MostrarGarage();
+//TODO OK
+
+echo "<p style = 'color:red;' >hola en rojo</p>";
+echo "<p style = 'color:violet;' >hola en violeta</p>";
+echo "<p style = 'color:white; background-color: black;'>hola en blanco con fondo negro</p>";
